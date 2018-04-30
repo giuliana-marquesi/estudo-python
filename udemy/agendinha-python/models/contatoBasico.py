b889d214d6f5e85ea1a3b25476bfedac2b03a839
Giuliana Marquesi <giuliana@riseup.net>
2018-04-29 23:11:36 -0300

    terminada classe contatoBasico

--- udemy/agendinha-python/models/contatoBasico.py
+++ udemy/agendinha-python/models/contatoBasico.py
@@ -1,10 +1,10 @@
-from telefone import Telefone
+from models.telefone import Telefone
 
 class ContatoBasico:
 
     def __init__(self, nome=' '):
         self.nome = nome
-        self._telefones = []
+        self.__telefones = []
 
     @property
     def nome(self):
@@ -15,12 +15,12 @@ class ContatoBasico:
         self.__nome = nome
 
     def addToTelefones(self, telefone):
-        self._telefones.append(telefone)
+        self.__telefones.append(telefone)
 
     def getDados(self):
         dados = self.__nome + '\n'
 
-        for telefone in self._telefones:
+        for telefone in self.__telefones:
             dados += telefone.getTelefone() + '\n'
 
         return dados
--- udemy/agendinha-python/tests/contatoBasico.py
+++ udemy/agendinha-python/tests/contatoBasico.py
@@ -5,17 +5,35 @@ from models.contatoBasico import ContatoBasico
 contatinhos = {'11909878979': 'Telefone celular', '23456718': 'Telefone residencial', '1234-5678': 'Telefone residencial', 
             '119089-0989': 'Telefone comercial'}
 
-@pytest.fixture
-def telefoneobj(telefone, tipo):
-    return Telefone(telefone, tipo)
+contato = ContatoBasico()
 
 @pytest.fixture
-def contato():
-    return ContatoBasico()
-
-def test_nenhumTelefone(contato):
-    contato.nome('Clodoaldo')
+def telefone():
+    contatoDaLista = contatinhos.popitem()
+    telefone = Telefone(contatoDaLista[0],contatoDaLista[1])
+    return telefone
+    
+def test_nenhumTelefone():
+    contato.nome = 'Clodoaldo'
  
-    assert contato.nome() == 'Clodoaldo'
-    assert contato.getDados() == 'Clodoaldo'
+    assert contato.nome == 'Clodoaldo'
+    assert contato.getDados() == 'Clodoaldo\n'
 
+def test_umTelefone(telefone):
+    contato.addToTelefones(telefone)
+    assert contato.getDados() == 'Clodoaldo\n119089-0989 Telefone comercial\n'
+
+def test_doisTelefones(telefone):
+    contato.addToTelefones(telefone)
+    assert contato.getDados() == 'Clodoaldo\n119089-0989 Telefone comercial\n1234-5678 Telefone residencial\n'
+
+def test_mudaNome():
+    contato.nome = 'Marcela'
+
+    assert contato.nome == 'Marcela'
+    assert contato.getDados() == 'Marcela\n119089-0989 Telefone comercial\n1234-5678 Telefone residencial\n'
+
+def test_insereNumDepoisMudancaNome(telefone):
+    contato.addToTelefones(telefone)
+    assert contato.getDados() == 'Marcela\n119089-0989 Telefone comercial\n1234-5678 Telefone residencial\n23456718 Telefone residencial\n'
+    
