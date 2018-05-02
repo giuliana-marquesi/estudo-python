7c073d8659ec9db7c44a0bdc12f416f572ec0399
Giuliana Marquesi <giuliana@riseup.net>
2018-05-01 21:25:18 -0300

    deletados arquivos desnecessarios

--- udemy/agendinha-python/models/contatoComercial.py
+++
@@ -1,9 +0,0 @@
-class ContatoComercial:
-	def __init__(self, atividade, funcionario, nome):
-		self.atividade = atividade
-		self.funcionario = funcionario
-		self.nome = nome
-
-	'''
-		definir getDados
-	'''
