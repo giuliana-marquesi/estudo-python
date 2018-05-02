b05915376677006f3a5ebcdb68d7fac5dc98d4d5
Giuliana Marquesi <giuliana@riseup.net>
2018-05-02 01:18:17 -0300

    atualizado os testes

--- udemy/agendinha-python/tests/lembrete.py
+++ udemy/agendinha-python/tests/lembrete.py
@@ -23,3 +23,14 @@ def test_setDescription(lembrete):
     other_description = 'oieee'
     lembrete.descr = other_description
     assert lembrete.descr == 'oieee'
+
+def test_readLembrete(lembrete):
+    assert lembrete.readLembrete() == 'Data: 14/03/18\nDescrição: niver da gigik'
+
+def test_updateLembrete(lembrete):
+    other_date = datetime.date(1991,3,14)
+    other_description = 'nascimento da gigik'
+    lembrete.updateLembrete(other_date, other_description)
+    assert lembrete.date == datetime.date(1991,3,14)
+    assert lembrete.descr == 'nascimento da gigik'
+    assert lembrete.readLembrete() == 'Data: 14/03/91\nDescrição: nascimento da gigik'
