7f1589dcdd9610911aa2d1a50259d1d575be4165
giuliana-marquesi <giugianam@gmail.com>
2018-05-08 13:21:17 -0300

    arrumando testes por nenomeação dos metodos e saida de string

--- udemy/agendinha-python/tests/lembrete.py
+++ udemy/agendinha-python/tests/lembrete.py
@@ -8,29 +8,29 @@ def lembrete():
     lembrete = Lembrete(date_event, 'niver da gigik')
     return lembrete
 
-def test_getDate(lembrete):
+def test_get_date(lembrete):
     assert lembrete.date == datetime.date(2018,3,14)
 
-def test_getDescription(lembrete):
+def test_get_description(lembrete):
     assert lembrete.descr == 'niver da gigik'
 
-def test_setDate(lembrete):
+def test_set_date(lembrete):
     other_date = datetime.date(1991,3,14)
     lembrete.date = other_date
     assert lembrete.date == datetime.date(1991,3,14)
 
-def test_setDescription(lembrete):
+def test_set_description(lembrete):
     other_description = 'oieee'
     lembrete.descr = other_description
     assert lembrete.descr == 'oieee'
 
-def test_readLembrete(lembrete):
-    assert lembrete.readLembrete() == 'Data: 14/03/18\nDescrição: niver da gigik'
+def test_read_lembrete(lembrete):
+    assert lembrete.read_lembrete() == 'Data: 14/03/18 Descrição: niver da gigik'
 
-def test_updateLembrete(lembrete):
+def test_update_lembrete(lembrete):
     other_date = datetime.date(1991,3,14)
     other_description = 'nascimento da gigik'
-    lembrete.updateLembrete(other_date, other_description)
+    lembrete.update_lembrete(other_date, other_description)
     assert lembrete.date == datetime.date(1991,3,14)
     assert lembrete.descr == 'nascimento da gigik'
-    assert lembrete.readLembrete() == 'Data: 14/03/91\nDescrição: nascimento da gigik'
+    assert lembrete.read_lembrete() == 'Data: 14/03/91 Descrição: nascimento da gigik'
