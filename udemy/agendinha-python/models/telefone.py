7c805bd649ba4600700acb37215e20714167a8aa
Giuliana <giugianam@gmail.com>
2018-05-08 18:50:39 -0300

    removidos arquivos desnecessários

--- udemy/agendinha-python/models/telefone.py
+++
@@ -1,7 +0,0 @@
-class Telefone:
-    def __init__(self,numero,tipo):
-        self.numero = numero
-        self.tipo = tipo
-
-    def getTelefone(self):
-        return str(self.numero) + " " + str(self.tipo)
